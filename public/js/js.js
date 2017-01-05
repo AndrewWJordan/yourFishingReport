3667d6d0134ac82a9b0aa9dcd5529bdd49bb0fd9
Jordan <MAC@Jordan.fios-router.home>
2017-01-04 21:25:48 -0500

    Displaying records based on search

--- public/js/js.js
+++ public/js/js.js
@@ -1,16 +1,11 @@
 $(document).ready(function(){
-  var baseURL = 'https://data.ny.gov/resource/u3vi-zfp5.json?name=Esopus Creek';
-  $('#result-table').dynatable();
-  //$('#searchButton').on('click', function() {
+  // NY Recommended Fishing Streams and Rivers API
+  var baseURL = 'https://data.ny.gov/resource/u3vi-zfp5.json?name=';
+  $('#searchButton').on('click', function(e) {
+    e.preventDefault();
+    // Get users search input from header
     var name = $('#searchInput').val();
-    var apiURL = baseURL;
-  /*$.getJSON(apiCall, function(data) {
-        $('#result-table').dynatable({
-            dataset: {
-                records: data
-            }
-        });
-  });*/
+    var apiURL = baseURL + name;
   $.ajax({ 
     url: apiURL, 
     type: "GET", 
@@ -19,15 +14,11 @@ $(document).ready(function(){
       "$$app_token" : "pZcBApJcX7oEFWwizD4j3JB7Q" 
     } 
   }).done(function(data) { 
-    console.table(data);
     $('#result-table').dynatable({
       dataset: {
         records: data
       }
     });
-    /*$.each(data, function(i) {
-      $('#table-data').append(data[i].name + '<br>');
-    });*/
   }); 
+  });
 });
-//});
