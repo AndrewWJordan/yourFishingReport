56da8c2c1b2725c7e5f1fbc3ca3c74bf9e665d9a
Andrew Jordan <andrew.jordan@marist.edu>
2017-01-29 20:59:34 -0500

    Added form validator, lat/lng, UI changes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express'),
     app = express(),
     path = require('path'),
     less = require('less-middleware'), // handles routing
-    bodyParser = require('body-parser'); // more middleware for parsing form data
+    bodyParser = require('body-parser'), // more middleware for parsing form data
+    validator = require('express-validator'); // form validation 
+
 
 // Setting up MongoDB
 var MongoClient = require('mongodb').MongoClient
@@ -15,6 +17,8 @@ MongoClient.connect('mongodb://localhost:27017/yourfishingreport', function (err
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+
+app.use(validator());
 
 // compile and serve css
 app.use(less(path.join(__dirname, 'source', 'less'), {
@@ -31,16 +35,22 @@ app.use(less(path.join(__dirname, 'source', 'less'), {
     },
     force: true,
 }));
-// serve static content
+
+// serve static content in /public
 app.use("/", express.static(__dirname + '/public'));
 
 app.post('/results', function (req, res) {
+    
+    // Validating form input from report.html
+    //req.checkBody("leader_email", "Enter a valid email address.").isEmail();
+    
+    // Connecting to MongoDB
     MongoClient.connect('mongodb://localhost:27017/yourfishingreport', function (err, db) {
         db.collection('reports').save(req.body, function (err, result) {
         if (err) { 
             return console.log(err);
         }
-        console.log('saved report')
+        console.log('New report saved.')
         res.redirect('/')
     })
 })
